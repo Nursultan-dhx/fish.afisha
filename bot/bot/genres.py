a73faa9856d74d3ce4154894cc8ab98f48ffdf4b
kseroxwq1 <nuptulladostan@gmail.com>
2026-05-13 22:08:38 +0300

    Add movie genre data structure

---
+++ bot/bot/genres.py
@@ -0,0 +1,16 @@
+GENRES = {
+    "28": "🔥 Action",
+    "12": "🗺 Adventure",
+    "35": "😂 Comedy",
+    "80": "🔫 Crime",
+    "18": "🎭 Drama",
+    "27": "👻 Horror",
+    "9648": "🔍 Mystery",
+    "10749": "❤️ Romance",
+    "878": "🚀 Science Fiction",
+    "53": "😰 Thriller",
+    "10752": "⚔️ War",
+    "37": "🤠 Western",
+    "99": "🎥 Documentary",
+    "10402": "🎵 Music",
+}
